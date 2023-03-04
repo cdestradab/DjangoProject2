e2b530df7cec3a7bdc9a97bc3b343979356cbf58
cdestradab <cdestradab@gmail.com>
2023-03-04 00:15:01 -0500

    for loops parte 1

--- my_site/my_app/views.py
+++ my_site/my_app/views.py
@@ -8,7 +8,7 @@ def example_view(request):
 def variable_view(request):
 
     my_var = {'first_name': 'roseLine', 'last_name': 'Franklin',
-              'some_list':[1,2,3], 'some_dict':{'inside_key':'inside_value'}
+              'some_list':[1,2,3], 'user_logged_in':True
               }
 
     return render(request, 'my_app/variable.html', context=my_var)
